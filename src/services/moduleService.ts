79586d96616722552ebeeb444461155123e4f4c9
Kristian <kristianratnikov28@gmail.com>
2024-11-07 17:45:18 +0000

    Added new requests to ModuleService to add update and delete entries from the module table

--- src/services/moduleService.ts
+++ src/services/moduleService.ts
@@ -6,3 +6,35 @@ export const getModules = async (): Promise<Module[]> => {
   console.log(response);
   return response.data;
 };
+export const addModule = async (module: Module) => {
+  try {
+    const response = await apiClient.post("/modules", module, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const updateModule = async (moduleId: number, module: Module) => {
+  try {
+    const response = await apiClient.put(`/modules/${moduleId}`, module, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const deleteModule = async (moduleid: number) => {
+  try {
+    const response = await apiClient.delete(`/modules/${moduleid}`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
