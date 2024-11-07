b6f53e5525c51af8f6b99dcb7759ca7b176a9e00
Kristian <kristianratnikov28@gmail.com>
2024-11-07 17:45:39 +0000

    Added new requests to ModuleService to add update and delete entries from the module table

--- src/services/studentService.ts
+++ src/services/studentService.ts
@@ -6,3 +6,44 @@ export const getStudents = async (): Promise<Student[]> => {
   console.log(response.data);
   return response.data;
 };
+
+export const deleteStudent = async (studentId: number): Promise<any> => {
+  const response = await apiClient.delete(`/students/${studentId}`);
+  console.log(response);
+  return response;
+};
+export const updateStudent = async (
+  studentId: number,
+  updatedStudent: Student
+): Promise<any> => {
+  try {
+    console.log(updatedStudent);
+    const response = await apiClient.put(
+      `/students/${studentId}`,
+      updatedStudent,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response.data; // Return response data if necessary
+  } catch (error) {
+    console.error("Error updating student:", error);
+    throw error; // Rethrow error so that handleSaveUpdatedStudent can handle it
+  }
+};
+export const addStudent = async (student: Student): Promise<any> => {
+  try {
+    console.log(student);
+    const response = await apiClient.post("/students", student, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error adding new student: ", error);
+    throw error;
+  }
+};
